66c1f29f0d9463bc5a4a6ab63e22b1da32fdda21
Ivis4ml <xxz220@miami.edu>
2019-11-11 12:06:35 -0500

    first commit

---
+++ R/PRScc-package.R
@@ -0,0 +1,11 @@
+##' Solving the Convex Clustering Algorithm and Sparse Convex Clustering Algorithm
+##' using Proximal Gradient Descent algorithm with Smooth Approximal scheme.
+##'
+##' 1/2 | X_i - U_i |_2^2 + \lambda \sum_{i < j} |U_i - U_j|_2 + \gamma \sum_{k} |U_{i,}|_2
+##'
+##' @docType package
+##' @name PRScc
+##' @aliases package-PRSCC
+##' @useDynLib PRScc
+##' @import Rcpp methods
+NULL
